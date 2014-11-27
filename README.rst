3b3f24b2309d7898989ae71a079b45a01cde977e
faassen <faassen@startifact.com>
2014-11-27 15:55:46 +0100

    Initial skeleton of project.

---
+++ README.rst
@@ -0,0 +1,9 @@
+confidant: a configuration engine for Python frameworks
+=======================================================
+
+Confidant is a powerful configuration engine for Python frameworks.
+
+It is used by Morepath_.
+
+.. _Morepath: http://morepath.readthedocs.org
+
---
+++ setup.py
@@ -0,0 +1,40 @@
+import os
+from setuptools import setup, find_packages
+
+long_description = (
+    open('README.rst').read()
+    + '\n' +
+    open('CHANGES.txt').read())
+
+setup(name='confidant',
+      version='0.1.dev0',
+      description="A configuration engine for Python frameworks",
+      long_description=long_description,
+      author="Martijn Faassen",
+      author_email="faassen@startifact.com",
+      url='http://confidant.readthedocs.org',
+      license="BSD",
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Environment :: Web Environment',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'Topic :: Internet :: WWW/HTTP :: WSGI',
+        'Programming Language :: Python :: 2.7',
+        'Development Status :: 4 - Beta'
+        ],
+      keywords="configuration",
+      install_requires=[
+        'setuptools',
+        ],
+      extras_require = dict(
+        test=['pytest >= 2.5.2',
+              'py >= 1.4.20',
+              'pytest-cov',
+              'pytest-remove-stale-bytecode',
+          ],
+        ),
+      )
