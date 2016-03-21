138865daa7adb456e873323d09c4d3e03f87beec
faassen <faassen@startifact.com>
2016-03-21 17:52:38 +0100

    A few more tests.

--- confidant/tests/test_directive.py
+++ confidant/tests/test_directive.py
@@ -1,8 +1,11 @@
 from confidant.app import App, Directive
 from confidant.config import Config
+from confidant.error import ConflictError
+
+import pytest
 
 
-def test_directive():
+def test_directive_main():
     config = Config()
 
     l = []
@@ -29,3 +32,73 @@ def test_directive():
     config.commit()
 
     assert l == ['hello']
+
+
+def test_directive_conflict():
+    config = Config()
+
+    l = []
+
+    @App.directive('foo')
+    class MyDirective(Directive):
+        def __init__(self, app, message):
+            super(MyDirective, self).__init__(app)
+            self.message = message
+
+        def identifier(self, registry):
+            return self.message
+
+        def perform(self, registry, obj):
+            l.append(self.message)
+
+    class MyApp(App):
+        testing_config = config
+
+    @MyApp.foo('hello')
+    def f():
+        pass
+
+    @MyApp.foo('hello')
+    def f2():
+        pass
+
+    with pytest.raises(ConflictError):
+        config.commit()
+
+
+def test_directive_override():
+    config = Config()
+
+    @App.directive('foo')
+    class MyDirective(Directive):
+        def __init__(self, app, message):
+            super(MyDirective, self).__init__(app)
+            self.message = message
+
+        def identifier(self, registry):
+            return self.message
+
+        def perform(self, registry, obj):
+            registry.message = self.message
+            registry.obj = obj
+
+    class MyApp(App):
+        testing_config = config
+
+    class SubApp(MyApp):
+        testing_config = config
+
+    @MyApp.foo('hello')
+    def f():
+        pass
+
+    @SubApp.foo('hello')
+    def f2():
+        pass
+
+    config.commit()
+
+    assert MyApp.registry.message == 'hello'
+    assert MyApp.registry.obj is f
+    assert SubApp.registry.message == 'hello'
+    assert SubApp.registry.obj is f2
