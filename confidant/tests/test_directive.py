b7eead1fb9d50f7aef0c18d0f1ccdbdb29a46e85
faassen <faassen@startifact.com>
2016-03-23 12:38:54 +0100

    A few tests for configuration groups.

--- confidant/tests/test_directive.py
+++ confidant/tests/test_directive.py
@@ -164,3 +164,119 @@ def test_directive_override():
     assert MyApp.configurations.my.obj is f
     assert SubApp.configurations.my.message == 'hello'
     assert SubApp.configurations.my.obj is f2
+
+
+def test_directive_different_group():
+    config = Config()
+
+    class Registry(object):
+        def __init__(self):
+            self.l = []
+
+        def add(self, message, obj):
+            self.l.append((message, obj))
+
+    @App.directive('foo')
+    class FooDirective(Action):
+        configurations = {
+            'foo': Registry
+        }
+
+        def __init__(self, message):
+            self.message = message
+
+        def identifier(self, registry):
+            return self.message
+
+        def perform(self, obj, foo):
+            foo.add(self.message, obj)
+
+    @App.directive('bar')
+    class BarDirective(Action):
+        configurations = {
+            'bar': Registry
+        }
+
+        def __init__(self, message):
+            self.message = message
+
+        def identifier(self, registry):
+            return self.message
+
+        def perform(self, obj, bar):
+            bar.add(self.message, obj)
+
+    class MyApp(App):
+        testing_config = config
+
+    @MyApp.foo('hello')
+    def f():
+        pass
+
+    @MyApp.bar('hello')
+    def g():
+        pass
+
+    config.commit()
+
+    MyApp.configurations.foo.l == [('hello', f)]
+    MyApp.configurations.bar.l == [('hello', g)]
+
+
+def test_directive_same_group():
+    config = Config()
+
+    class Registry(object):
+        def __init__(self):
+            self.l = []
+
+        def add(self, message, obj):
+            self.l.append((message, obj))
+
+    @App.directive('foo')
+    class FooDirective(Action):
+        configurations = {
+            'foo': Registry
+        }
+
+        def __init__(self, message):
+            self.message = message
+
+        def identifier(self, registry):
+            return self.message
+
+        def perform(self, obj, foo):
+            foo.add(self.message, obj)
+
+    @App.directive('bar')
+    class BarDirective(Action):
+        configurations = {
+            'bar': Registry
+        }
+
+        def __init__(self, message):
+            self.message = message
+
+        # should now conflict
+        def group_key(self):
+            return FooDirective
+
+        def identifier(self, registry):
+            return self.message
+
+        def perform(self, obj, bar):
+            bar.add(self.message, obj)
+
+    class MyApp(App):
+        testing_config = config
+
+    @MyApp.foo('hello')
+    def f():
+        pass
+
+    @MyApp.bar('hello')
+    def g():
+        pass
+
+    with pytest.raises(ConflictError):
+        config.commit()
