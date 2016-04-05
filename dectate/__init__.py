343deef9dfc932bb324308b1a9d92ebc4ffcbf55
faassen <faassen@startifact.com>
2016-04-05 13:59:00 +0200

    Expose a few compare functions.

--- dectate/__init__.py
+++ dectate/__init__.py
@@ -3,5 +3,5 @@ from .app import App, autocommit
 from .config import commit, Action, Composite, CodeInfo
 from .error import (ConfigError, DirectiveError, TopologicalSortError,
                     DirectiveReportError, ConflictError, QueryError)
-from .query import Query, execute
+from .query import Query, execute, compare_equality, compare_subclass
 from .tool import querytool
