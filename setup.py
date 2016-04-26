5fef04268962058854c817f3117e274b3b1aab5d
faassen <faassen@startifact.com>
2016-04-26 15:19:46 +0200

    Back to development: 0.11

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (
     open('CHANGES.txt').read())
 
 setup(name='dectate',
-      version='0.10.2',
+      version='0.11.dev0',
       description="A configuration engine for Python frameworks",
       long_description=long_description,
       author="Martijn Faassen",
