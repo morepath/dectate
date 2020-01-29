45bda696390e0345502a2e75f84db8f618220cb1
Henri Hulski <henri.hulski@gazeta.pl>
2020-01-29 23:07:40 +0100

    Preparing release 0.14

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ long_description = "\n".join(
 
 setup(
     name="dectate",
-    version="0.14.dev0",
+    version="0.14",
     description="A configuration engine for Python frameworks",
     long_description=long_description,
     author="Martijn Faassen",
