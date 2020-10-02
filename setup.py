5e479dae9d7b0a55ed74dd686de7b51469e040ed
Jürgen Gmach <juergen.gmach@googlemail.com>
2020-10-02 10:55:47 +0200

    Maintenance work
    - drop support for Python 3.4 and 3.5
    - apply Black code formatter
    - fix Flake8 erros
    
    modified:   .travis.yml
    modified:   CHANGES.txt
    modified:   dectate/config.py
    modified:   dectate/error.py
    modified:   dectate/tool.py
    modified:   setup.py
    modified:   tox.ini

--- setup.py
+++ setup.py
@@ -24,8 +24,6 @@ setup(
         "Intended Audience :: Developers",
         "License :: OSI Approved :: BSD License",
         "Topic :: Software Development :: Libraries :: Application Frameworks",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
